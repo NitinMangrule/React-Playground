b48347e445ce1d027eb86ffcea4f4ac9e54745b2
Nitin Mangrule <ndmangrule@gmail.com>
2023-02-04 20:09:59 +0530

    Added chapter 3

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+    "h1",
+    {
+      id: "title",
+      style: {color:'red',
+            backgroundColor: '#eeee'    
+            }
+    },
+    "Helloo World.."
+  );
+  const heading2 = React.createElement(
+    "h2",
+    {
+      id: "title",
+      style: {color:'skyblue',
+            backgroundColor: '#eeee'    
+            }
+    },
+    "Helloo India"
+  );
+  const container = React.createElement(
+    "div",
+    {
+      id: "container"
+    },
+    [heading, heading2]
+  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // Passing react element inside root.
+  root.render(container);
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # React-Playground
  
+commands
+> npm i -D parcel
+> npm i react
+> npm i react-dom
+
+#To start server
+> npx parcel index.html
+# For build > npx parcel build index.html 
+index.html is entry point for build
