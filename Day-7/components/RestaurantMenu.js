2690b307a2057109114d6d4c77bee6b14a70a1a7
Nitin Mangrule <ndmangrule@gmail.com>
2023-03-18 13:55:57 +0530

    Updated api url

--- Day-7/components/RestaurantMenu.js
+++ Day-7/components/RestaurantMenu.js
@@ -13,12 +13,12 @@ const RestaurantMenu = () => {
 
   async function getRestaurantInfo() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/v4/full?lat=18.5236317&lng=73.8411226&menuId=" +
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5236317&lng=73.8411226&restaurantId=" +
         id
     );
     const json = await data.json();
-    console.log(json);
-    setRestaurantData(json.data);
+    console.log('123',json.data.cards[2].groupedCard.cardGroupMap.REGULAR);
+    setRestaurantData(json.data.cards);
   }
 
   return !restaurantData ? (
@@ -26,24 +26,24 @@ const RestaurantMenu = () => {
   ) : (
     <div className="center">
       <div>
-        <h1>{restaurantData.name}</h1>
+        <h1>{restaurantData[0].card.card.info.name}</h1>
         <img
-          src={IMG_CDN_URL + restaurantData?.cloudinaryImageId}
+          src={IMG_CDN_URL + restaurantData[0].card.card.info?.cloudinaryImageId}
           style={{ borderRadius: "40px" }}
         />
-        <h2>{restaurantData?.area}</h2>
-        <h3>{restaurantData?.city}</h3>
-        <Rating readonly initialValue={restaurantData?.avgRating} />
-        <h3>{restaurantData?.costForTwoMsg}</h3>
+        <h2>{restaurantData[0].card.card.info?.area}</h2>
+        <h3>{restaurantData[0].card.card.info?.city}</h3>
+        <Rating readonly initialValue={restaurantData[0].card.card.info?.avgRating} />
+        <h3>{restaurantData[0].card.card.info?.costForTwoMsg}</h3>
       </div>
       <div>
         <h1>Menu</h1>
         <main className="leaderboard__profiles">
-          {Object.values(restaurantData?.menu?.items).map((item) => (
-            <article className="leaderboard__profile" key={item?.id}>
-              {getImageBlock(item?.cloudinaryImageId)}
-              <span className="leaderboard__name">{item?.name}</span>
-              <span className="leaderboard__value">Rs.{item?.price / 100}</span>
+          {restaurantData[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card.itemCards.map((item) => (
+            <article className="leaderboard__profile" key={item?.card?.info.id}>
+              {getImageBlock(item?.card?.info.imageId)}
+              <span className="leaderboard__name">{item?.card?.info.name}</span>
+              <span className="leaderboard__value">Rs.{item?.card?.info.price / 100}</span>
             </article>
           ))}
         </main>
@@ -55,7 +55,7 @@ const RestaurantMenu = () => {
 const getImageBlock = (cloudinaryImageId) => {
   return cloudinaryImageId ? (
     <img
-      src={IMG_CDN_URL + cloudinaryImageId}
+      src={'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/' + cloudinaryImageId}
       className="leaderboard__picture"
     />
   ) : (
